361776f8af755d90a1e7fcd6771adf350974f103
meeansub <welcltm@gmail.com>
2020-07-04 18:49:41 +0900

    push

---
+++ Rush00/Rush00.c
@@ -0,0 +1,38 @@
+void ft_putchar(char c);
+
+
+void		make_col(int x,char *input){
+    int col;
+    col=0;
+    while(col < x){
+        if(col ==0)
+            ft_putchar(input[0]);
+        else if(col == x-1)
+            ft_putchar(input[1]);
+        else
+            ft_putchar(input[2]);
+       
+        col++;
+    }
+    ft_putchar('\n');
+
+}
+
+void		make_row(int x, int y){
+    int row;
+    row = 0;
+
+	char *input = {"oo-|| "};
+    while(row < y){
+        if(row == 0 || row ==y-1)
+            make_col(x,input);
+        else
+            make_col(x,&input[3]);
+		row++;
+    }
+
+}
+void		rush(int x, int y){
+	make_row(x,y);
+
+}
---
+++ Rush00/main.c
@@ -0,0 +1,20 @@
+void rush(int x,int y);
+void ft_putchar(char c);
+
+int 		main(){
+	rush(5,5);	
+	
+	ft_putchar('\n');
+
+	rush(5,1);	
+
+	ft_putchar('\n');
+
+	rush(1,1);
+
+	ft_putchar('\n');
+
+	rush(1,5);
+	
+
+}
