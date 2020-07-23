87c979a882028216fbad342ea71cf070d018184d
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-07-23 13:43:52 +0900

    update

--- exam/doit/rev_print.c
+++ exam/doit/rev_print.c
@@ -1,13 +1,38 @@
+#include <unistd.h>
+
+void	rev_print(char *str)
+{
+	int size = 0;
+	while(str[size])
+		size++;
+	
+	while(size--)
+	{
+		write(1, &str[size], 1);
+	}
+}
+
+int main(int ac, char **av)
+{
+	if(ac == 1)
+	{
+		rev_print(av[1]);
+	}
+	else
+		write(1, "\n", 1);
+}
+
+
 /*
 Assignment name  : rev_print
 Expected files   : rev_print.c
 Allowed functions: write
 --------------------------------------------------------------------------------
 
-Write a program that takes a string, and displays the string in reverse
-followed by a newline.
+문자열을 가져 와서 문자열을 반대로 표시하는 프로그램을 작성하십시오.
+줄 바꿈이 뒤 따릅니다.
 
-If the number of parameters is not 1, the program displays a newline.
+매개 변수 수가 1이 아닌 경우 프로그램은 개행을 표시합니다.
 
 Examples:
 
