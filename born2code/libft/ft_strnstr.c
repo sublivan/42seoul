0a30c20539674051589eb6f3993db6d90b56bf3f
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-10-15 17:36:14 +0900

    libft part1 - strnstr

---
+++ born2code/libft/ft_strnstr.c
@@ -0,0 +1,27 @@
+#include "libft.h"
+
+char	*strnstr(const char *big, const char *little, size_t len)
+{
+	size_t	i;
+	size_t	j;
+
+	if (little == '\0')
+		return ((char *)big);
+	i = 0;
+	while (big[i] && i < len)
+	{
+		j = 0;
+		while (big[i + j] && little[j] && i + j < len)
+		{
+			if (big[i + j] != little[j])
+				break;
+			j++;
+		}
+		if (little[j] == '\0')
+		{
+			return ((char *)big + i);
+		}
+		i++;
+	}
+	return (NULL);
+}
