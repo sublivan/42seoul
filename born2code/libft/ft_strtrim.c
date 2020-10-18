d614ec20f90a1880abc457d0b2229b0abdbc498d
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-10-19 00:14:33 +0900

    libft - part2

---
+++ born2code/libft/ft_strtrim.c
@@ -0,0 +1,41 @@
+#include "libft.h"
+
+int		str_in_set(char c, char const *set)
+{
+	size_t i;
+
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	char *result;
+	size_t start;
+	size_t end;
+	size_t i;
+
+	start = 0;
+	while (s1[start] && str_in_set(s1[start], set))
+	{
+		start++;
+	}
+	end = ft_strlen(s1);
+	while (end > start && str_in_set(s1[end - 1], set))
+		end--;
+	if (!(result = malloc(end - start + 1)))
+		return (NULL);
+	i = 0;
+	while (start < end)
+	{
+		result[i++] = s1[start++];
+	}
+	result[i] = '\0';
+	return (result);
+}
