8defb252f0e032c432b371cfbfa351ce1f74d054
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-10-19 21:41:30 +0900

    update

---
+++ born2code/libft/ft_strmapi.c
@@ -0,0 +1,22 @@
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	size_t	s_len;
+	size_t	i;
+	char	*new_str;
+
+	if (!s || !f)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (!(new_str = (char *)malloc(sizeof(char) * (len + 1))))
+		return (NULL);
+	i = 0;
+	while (s[i])
+	{
+		new_str[i] = f(i, s[i]);
+		i++;
+	}
+	new_str[i] = '\0';
+	return (new_str);
+}
