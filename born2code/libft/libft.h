a3621f9d1666185c46e0067a1a39a3b1c7571cba
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-10-20 17:06:47 +0900

    libft.h

--- born2code/libft/libft.h
+++ born2code/libft/libft.h
@@ -16,17 +16,12 @@
 # include <unistd.h>
 # include <stdlib.h>
 
-void	*ft_bzero(void *str, size_t n);
-void	*ft_memccpy(void *destination, const void *source, int c, size_t num);
-void	*ft_memchr(const void *ptr, int value, size_t num);
-int		ft_memcmp(const void *ptr1, const void *ptr2, size_t num);
-void	*ft_memcpy(void *destination, const void *source, size_t num);
-void	*ft_memmove(void *destination, const void *source, size_t num);
-void	*ft_memset(void *ptr, int value, size_t num);
-size_t	ft_strlcpy(char *dest, const char *src, size_t dest_size);
+typedef struct s_list
+{
+	void			*content;
+	struct	s_list	*next;
+}				t_list;
+
 size_t	ft_strlen(const char *str);
-char	*ft_strrchr(const char *str, int ch);
-int		ft_isalpha(int c);
-int		ft_isdigit(int c);
-void	ft_putchar_fd(char c, int fd);
+
 #endif
