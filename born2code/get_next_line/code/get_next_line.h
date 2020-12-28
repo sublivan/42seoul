dcc775cbc06371ce00b1cf04ce6ab3c055ae8f0a
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-12-28 17:48:18 +0900

    style: 들여쓰기수정

--- born2code/get_next_line/code/get_next_line.h
+++ born2code/get_next_line/code/get_next_line.h
@@ -10,12 +10,12 @@
 #  define BUFFER_SIZE 1024
 # endif
 
-int get_next_line(int fd, char **line);
+int		get_next_line(int fd, char **line);
 
-size_t  ft_strlen(const char *s);
-char    *ft_strchr(const char *s, int c);
-size_t  ft_strlcpy(char *dest, const char *src, size_t dstsize);
-char    *ft_strdup(const char *s1);
-char    *ft_strjoin(char const *s1, char const *s2);
+size_t	ft_strlen(const char *s);
+char	*ft_strchr(const char *s, int c);
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
+char	*ft_strdup(const char *s1);
+char	*ft_strjoin(char const *s1, char const *s2);
 
 #endif
