3d4e5df336e2490c8499df314e65756d6dba13b4
김민섭 <33247303+meeansub@users.noreply.github.com>
2020-12-28 22:01:16 +0900

    feat: 정적변수 추가

--- born2code/get_next_line/get_next_line.h
+++ born2code/get_next_line/get_next_line.h
@@ -7,7 +7,11 @@
 # include <limits.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 1024
+#  define BUFFER_SIZE 5000
+# endif
+
+# ifndef OPEN_MAX
+#  define OPEN_MAX 32
 # endif
 
 int		get_next_line(int fd, char **line);
